7b5228a2f3a1294365604153c542c1be69485b22
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-04-28 14:58:04 +0430

    templates is finished

---
+++ blog/factory.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+import factory
+from . models import Post
+from factory.faker import faker
+
+FAKE = faker.Faker()
+class PostFactory(factory.Factory):
+    class Meta:
+        model = Post
+    title = factory.Faker("sentence",nb_words=12)
+    subtitle = factory.Faker("sentence",nb_works=12)
+    slug = factory.Faker("slug")
+    author = User.objects.get_or_create(username='admin')[0]
+    @factory.LazyAttribute()
+    def content(self):
+        x =  ''
+        for _ in range(0.5):
+            x += "\n" + FAKE.paragraph(nb_sentences=30) + "\n"
+        return x
+    status = 'Published'
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from . models import Post
+from django.views.generic import ListView
 from django.http import HttpResponse
 
 
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    
 def home(request):
     return render(request, 'blog/index.html')
