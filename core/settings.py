281b10be062d2454ccb41eb29a766e2dd9302d50
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-04-28 08:14:12 +0430

    templates is added

--- core/settings.py
+++ core/settings.py
@@ -116,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [
+    'core/static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
