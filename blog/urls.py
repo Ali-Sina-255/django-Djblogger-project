c8b7e9fd19a73d0c100a83b40d2ff667aa536a21
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-05-02 20:49:09 +0430

    tag page is created

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('post/<slug:slug>/', views.single_post_page, name='single_page'),
+    path('tag/<slug:tag>/', views.TagViewList.as_view(), name='tag_boy_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from . models import Post
 from django.views.generic import ListView
@@ -29,3 +31,20 @@ def single_post_page(request, slug):
         "related": related
     }
     return render(request, 'blog/single_Post.html', context)
+
+class TagViewList(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 6
+    
+    def get_queryset(self):
+        return Post.objects.filter(tags__name__in=[self.kwargs['tag']])
+    def get_template_names(self):
+        if self.request.htmx:
+            return "components/post-list-tag.html"
+        return 'blog/tags.html'
+    def get_context_data(self, **kwargs):
+        context = super(TagViewList, self).get_context_data(**kwargs)
+        context['tag'] = self.kwargs['tag']
+        return context
+        
